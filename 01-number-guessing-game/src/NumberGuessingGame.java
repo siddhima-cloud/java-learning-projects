05a2fc6fae317d8c1f596bb1c8581edec0be059b
Siddhima Tiwari <siddhimaishere@gmail.com>
2026-06-30 22:48:52 +0530

    This version of the game allows you to replay game

--- 01-number-guessing-game/src/NumberGuessingGame.java
+++ 01-number-guessing-game/src/NumberGuessingGame.java
@@ -1,38 +1,61 @@
+import java.util.Scanner;
+
 public class NumberGuessingGame {
 
     public static void guessNumber() {
-        int secretNumber = (int) (Math.random() * 100) + 1;
-        int guess = 0;
-        java.util.Scanner sc = new java.util.Scanner(System.in);
-        int attempts = 5;
 
-        System.out.println("Welcome to the Number Guessing Game!");
-        System.out.println("I have selected a number between 1 and 100.");
-        System.out.println("You have " + attempts + " attempts to guess the number.");
+        final int MAX_ATTEMPTS = 5;
+        char playAgain;
 
-        do {
-            System.out.print("Enter your guess: ");
-            guess = sc.nextInt();
-            attempts--;
+        try (Scanner scanner = new Scanner(System.in)) {
 
-            if (guess < secretNumber) {
-                System.out.println("Too low! Try again.");
-            } else if (guess > secretNumber) {
-                System.out.println("Too high! Try again.");
-            } else {
-                System.out.println("Congratulations! You've guessed the number!");
-                break;
-            }
+            System.out.println("=================================");
+            System.out.println(" Welcome to the Number Guessing Game");
+            System.out.println("=================================");
 
-            if (attempts == 0) {
-                System.out.println("Sorry, you've run out of attempts. The number was: " + secretNumber);
-            } else {
-                System.out.println("You have " + attempts + " attempts left.");
-            }
+            do {
 
-        } while (attempts > 0);
+                int secretNumber = (int) (Math.random() * 100) + 1;
+                int remainingAttempts = MAX_ATTEMPTS;
+                boolean hasWon = false;
 
-        sc.close();
+                System.out.println("\nI have selected a number between 1 and 100.");
+                System.out.println("You have " + MAX_ATTEMPTS + " attempts.");
+
+                while (remainingAttempts > 0) {
+
+                    System.out.print("Enter your guess: ");
+                    int userGuess = scanner.nextInt();
+
+                    remainingAttempts--;
+
+                    if (userGuess < secretNumber) {
+                        System.out.println("Too low! Try again.");
+                    } else if (userGuess > secretNumber) {
+                        System.out.println("Too high! Try again.");
+                    } else {
+                        System.out.println("Congratulations! You guessed the number.");
+                        hasWon = true;
+                        break;
+                    }
+
+                    if (remainingAttempts > 0) {
+                        System.out.println("Remaining attempts: " + remainingAttempts);
+                    }
+                }
+
+                if (!hasWon) {
+                    System.out.println("Sorry! You have used all your attempts.");
+                    System.out.println("The correct number was: " + secretNumber);
+                }
+
+                System.out.print("\nDo you want to play again? (y/n): ");
+                playAgain = scanner.next().charAt(0);
+
+            } while (playAgain == 'y' || playAgain == 'Y');
+
+            System.out.println("\nThank you for playing!");
+        }
     }
 
     public static void main(String[] args) {
